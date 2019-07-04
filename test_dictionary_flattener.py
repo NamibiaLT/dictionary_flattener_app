7f05c3089fd587c62f1c679c8eea3ba8160edbeb
NLT <NamibiaLT@users.noreply.github.com>
2019-07-04 12:09:44 -0700

    write test

--- test_dictionary_flattener.py
+++ test_dictionary_flattener.py
@@ -1,11 +1,23 @@
 import dictionary_flattener as dif
 
 sample_dict1 = {
-  test: 1,
-  stuff: {
-    a: 1,
-    b: 2
+  'test': 1,
+  'stuff': {
+    'a': 1,
+    'b': 2
   },
-  nums: [4,5,6[7,8]]
+  'nums': [4,5,6[7,8]]
 }
 
+
+def test_flatten_dict():
+    dif.flatten_dict(sample_dict1) == {
+        'test': 1,
+        'stuff.a': 1,
+        'stuff.b': 2,
+        'nums.0': 4,
+        'nums.1': 5,
+        'nums.2': 6,
+        'nums.3.0': 7,
+        'nums.3.1': 8
+    }
